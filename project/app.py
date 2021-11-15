d9659de1e717ed050fd2a0b5ba4e1914a7f03a92
Ledon <s274181@studenti.polito.it>
2021-11-15 23:59:09 +0800

    upload dockerfile
    upload dockerfile and make some changes on app.py to expose port and update readme

--- project/app.py
+++ project/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, session
 from flask_bootstrap import Bootstrap
-
-
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -173,4 +171,4 @@ def updatetime():
     pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host=('0.0.0.0'))
