654573f1a22e84f2e3a8b81cc1b43366938d92e4
shantifabri <sfabri1@gmail.com>
2021-11-28 14:29:22 +0100

    add home tests and fix url_for(index)

--- project/tests/test_routes.py
+++ project/tests/test_routes.py
@@ -8,4 +8,16 @@ def test_home_page(test_client):
     assert response.status_code == 200
     assert b"Oner SPG" in response.data
     assert b"Order fresh and genuine products everyday" in response.data
-    assert b"Existing user?" not in response.data
+    assert b"Log In" in response.data
+    assert b"Sign Up" in response.data
+    assert b"Log Out" not in response.data
+
+def test_home_page_post(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is is posted to (POST)
+    THEN check that a '405' status code is returned
+    """
+    response = test_client.post('/')
+    assert response.status_code == 405
+    assert b"Order fresh and genuine products everyday" not in response.data
