cccf435253c84bae43820e03079ac4302b17e154
shantifabri <sfabri1@gmail.com>
2021-12-10 19:57:29 +0100

    add products tests

--- project/tests/test_routes.py
+++ project/tests/test_routes.py
@@ -1,3 +1,5 @@
+from flask import url_for, request
+
 def test_home_page(test_client):
     """
     GIVEN a Flask application configured for testing
@@ -15,7 +17,7 @@ def test_home_page(test_client):
 def test_home_page_post(test_client):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/' page is is posted to (POST)
+    WHEN the '/' page is posted to (POST)
     THEN check that a '405' status code is returned
     """
     response = test_client.post('/')
@@ -121,14 +123,41 @@ def test_products_page_logged(test_client, init_database, login_employee_user):
     """
     response = test_client.get('/products')
     assert response.status_code == 200
+    assert b"Categories" in response.data
     assert b"Vegetables" in response.data
     assert b"Fruit" in response.data
+    assert b'Bananas' in response.data
 
+def test_products_page_not_logged_redirects(test_client, init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/products' page is requested (GET) and user is not logged in
+    THEN check that it gets redirected to login and the response is valid
+    """
+    response = test_client.get('/products', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Categories" not in response.data
+    assert b"Please log in" in response.data
+
+    assert request.path == url_for('users.login')
+
+def test_products_page_unauthorized_logged_redirects(test_client, init_database, login_warehouse_employee_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/products' page is requested (GET) and user is logged in as unauthorized role
+    THEN check that it gets redirected to index and the response is valid
+    """
+    response = test_client.get('/products', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Categories" not in response.data
+    assert b"Order fresh and genuine products everyday" in response.data
+
+    assert request.path == url_for('other.index')
 
 def test_single_product_page_logged(test_client, init_database, login_employee_user):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/products' page is requested (GET) and user is logged in
+    WHEN the '/singleproduct/<id>' page is requested (GET) and user is logged in
     THEN check that the response is valid
     """
     response = test_client.get('/singleproduct/1')
@@ -138,3 +167,41 @@ def test_single_product_page_logged(test_client, init_database, login_employee_u
     assert b"Quantity" in response.data
     assert b"Add to Cart" in response.data
 
+def test_single_product_page_not_logged_redirects(test_client, init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/singleproduct/<id>' page is requested (GET) and user is not logged in
+    THEN check that it gets redirected to login and the response is valid
+    """
+    response = test_client.get('/singleproduct/1', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Farmer" not in response.data
+    assert b"Add to Cart" not in response.data
+    assert b"Please log in" in response.data
+
+    assert request.path == url_for('users.login')
+
+def test_single_product_page_unauthorized_logged_redirects(test_client, init_database, login_warehouse_employee_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/singleproduct/<id>' page is requested (GET) and user is logged in as unauthorized role
+    THEN check that it gets redirected to index and the response is valid
+    """
+    response = test_client.get('/singleproduct/1', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Add to Cart" not in response.data
+    assert b"Order fresh and genuine products everyday" in response.data
+
+    assert request.path == url_for('other.index')
+
+def test_single_product_page_post(test_client, init_database, login_employee_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/singleproduct/<id>' page is posted to (POST)
+    THEN check that it gets redirected to products after post and the response is valid
+    """
+    response = test_client.post('/singleproduct/1',
+                                data=dict(quantity=2),
+                                follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Categories" in response.data
