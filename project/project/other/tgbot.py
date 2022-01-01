d5a5fe4487160789306185616b76d60faed9a2e9
Ledon <s274181@studenti.polito.it>
2022-01-02 01:07:18 +0800

    add SPG-44
    add SPG-44

---
+++ project/project/other/tgbot.py
@@ -0,0 +1,37 @@
+from telepot.loop import MessageLoop
+import telepot
+import time
+import sqlite3
+from dotenv import load_dotenv, find_dotenv
+import os
+
+conn = sqlite3.connect('/Users/ledon/Downloads/SE2-R1-SPGdocker/project/instance/database.db')
+cursor = conn.cursor()
+cursor.execute('select tgusername,wallet,pending_amount from users where tgusername IS NOT NULL')
+values = cursor.fetchall()
+conn.close()
+
+load_dotenv(verbose=True)
+TOKEN = os.getenv("TOKEN")
+bot = telepot.Bot(TOKEN)
+
+def handle(msg):
+    username = msg['from']['username']
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(username)
+
+    if msg['text'] == '/balance':
+        for row in values:
+
+            if username == row[0]:
+                s = row[1]-row[2]
+                bot.sendMessage(chat_id, 'you balance is: %s euro' % (s))
+                break
+        else:
+            bot.sendMessage(chat_id, 'please bind your telegram username firstly')
+
+MessageLoop(bot, handle).run_as_thread()
+
+
+while 1:
+    time.sleep(5)
