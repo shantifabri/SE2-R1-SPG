3ecd65b4113d64b9e9de1e4fd86bbe45516fa8b8
Spidersaw <erew1@hotmail.it>
2022-01-08 20:26:09 +0100

    calendar fixes

--- project/project/static/assets/js/main.js
+++ project/project/static/assets/js/main.js
@@ -1,5 +1,10 @@
 $(function() {
 
-   $('#pick-date').pickadate();
+   $('#pick-date').pickadate({
+      format: 'dd/mm/yyyy',
+      formatSubmit: 'dd/mm/yyyy',
+      min: "20-03-2022",
+      max: "22-03-2022"
+   });
 
 });
