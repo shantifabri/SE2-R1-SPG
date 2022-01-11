4f7370bdd6e680fb980db015368f1f0910f68beb
Ledon <s274181@studenti.polito.it>
2022-01-11 16:51:34 +0800

    fix bugs for bot and dockerfile
    fix bugs for bot and dockerfile

--- project/project/other/tgbot.py
+++ project/project/other/tgbot.py
@@ -5,10 +5,15 @@ import sqlite3
 from dotenv import load_dotenv, find_dotenv
 import os
 
-conn = sqlite3.connect('/project/instance/database.db')
-cursor = conn.cursor()
-cursor.execute('select tg_chat_id,wallet,pending_amount from users where tg_chat_id IS NOT NULL')
-values = cursor.fetchall()
+
+def condb():
+    conn = sqlite3.connect("/spgr2/instance/database.db")
+    cursor = conn.cursor()
+    cursor.execute('select tg_chat_id,wallet,pending_amount from users where tg_chat_id IS NOT NULL')
+    result = cursor.fetchall()
+    return result
+    conn.close()
+
 
 load_dotenv(verbose=True)
 TOKEN = os.getenv("TOKEN")
@@ -21,16 +26,17 @@ def handle(msg):
         bot.sendMessage(chat_id, 'Your telegram user id is: %s, please give id to ShopEmployee' % (chat_id))
 
     if msg['text'] == '/balance':
+        values = condb()
         for row in values:
             if chat_id == row[0]:
-                s = row[1]-row[2]
-                bot.sendMessage(chat_id, 'you balance is: %d euro' % (s))
+                s = row[1] - row[2]
+                bot.sendMessage(chat_id, 'you balance is: %s euro' % (s))
                 break
         else:
             bot.sendMessage(chat_id, 'please bind your telegram username firstly')
 
-MessageLoop(bot, handle).run_as_thread()
 
+MessageLoop(bot, handle).run_as_thread()
 
 while 1:
     time.sleep(5)
